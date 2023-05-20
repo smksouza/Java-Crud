a380ea9042781c73175c448f6a50d936d64713f6
Samuel de Souza <smksouza.dev@gmail.com>
2023-05-20 14:54:26 -0300

    Projeto A3 da faculdade

--- src/main/java/com/mycompany/principal/java/PrincipalJava.java
+++ src/main/java/com/mycompany/principal/java/PrincipalJava.java
@@ -1,126 +1,144 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.principal.java;
 
-
-
 /**
  *
- * @author samue
+ * @author 12723112947
  */
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class PrincipalJava {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         ArrayList<Usuario> usuarios = new ArrayList();
 
-    boolean rodando = true;   
-    
-    while(rodando){
-        System.out.println("[1] - Cadastrar usuário");
-        System.out.println("[2] - Listar todos os usuários cadastrados");
-        System.out.println("[3] - Procurar usuário por email");
-        System.out.println("[4] - Atualizar dados de um usuario");
-        System.out.println("[5] - Remover usuário");
-        System.out.println("[6] - Sair!");
-        System.out.println("Digite a opção: ");
-        String opcao = input.nextLine();
-        
-        switch (opcao) {
+        boolean rodando = true;
+
+        while (rodando) {
+            System.out.println("==================================");
+            System.out.println("[1] - Cadastrar usuário");
+            System.out.println("[2] - Listar todos os usuários cadastrados");
+            System.out.println("[3] - Procurar usuário por email");
+            System.out.println("[4] - Atualizar dados de um usuario");
+            System.out.println("[5] - Remover usuário");
+            System.out.println("[6] - Sair!");
+            System.out.println("Digite a opção: ");
             
-            case "1": {
-                System.out.println("CADADSTRO DE USUÁRIO");
-                System.out.print("Digite o email: ");
-                String email = input.nextLine();
-                System.out.print("Digite a senha: ");
-                String senha = input.nextLine();
-                Usuario u = new Usuario();
-                u.setEmail(email);
-                u.setSenha(senha);
-            
-                usuarios.add(u);
-                System.out.println("O email cadastrado foi: " + u.getEmail());
-                System.out.println("A senha cadastrada foi: " + u.getSenha());
-                break;
-            }
-            case "2": {
-                System.out.println("TODOS OS USUÁRIOS CADASTRADOS");
-                for (int i = 0; i < usuarios.size(); i++) {
-                    Usuario uTemp = usuarios.get(i);
-                    System.out.println("Usuarío " + i);
-                    System.out.println("\tEmail: " + uTemp.getEmail());
-                    System.out.println("\tSenha: " + uTemp.getSenha());                  
+            String opcao = input.nextLine();
+
+            switch (opcao) {
+
+                case "1": {
+                    System.out.println("==========CADADSTRO DE USUÁRIO==========");
+                    System.out.print("Digite o email: ");
+                    String email = input.nextLine();
+                    System.out.print("Digite a senha: ");
+                    String senha = input.nextLine();
+                    System.out.print("Digite o nome: ");
+                    String nome = input.nextLine();
+                    System.out.print("Digite a idade: ");
+                    String idade = input.nextLine();
+                    System.out.print("Digite o CPF: ");
+                    String cpf = input.nextLine();
+
+                    Usuario u = new Usuario();
+                    u.setEmail(email);
+                    u.setSenha(senha);
+                    u.setNome(nome);
+                    u.setIdade(idade);
+                    u.setCpf(cpf);
+
+                    usuarios.add(u);
+                    System.out.println("O email cadastrado foi: " + u.getEmail());
+                    System.out.println("A senha cadastrada foi: " + u.getSenha());
+                    break;
                 }
-                break;
-            }
-            case "3": {
-                System.out.println("PROCURAR USUÁRIO POR EMAIL");
-                System.out.println("Digite o email:");
-                String email = input.nextLine();
-                boolean encontrado = false;
-                for (int i = 0; i < usuarios.size(); i++) {
-                    Usuario uTemp = usuarios.get(i);
-                    if (email.equals(uTemp.getEmail())) {
-                        System.out.println("Usuário encontrado");
+                case "2": {
+                    System.out.println("==========TODOS OS USUÁRIOS CADASTRADOS==========");
+                    for (int i = 0; i < usuarios.size(); i++) {
+                        Usuario uTemp = usuarios.get(i);
+                        System.out.println("\tUsuaríos Totais " + (i + 1));
                         System.out.println("\tEmail: " + uTemp.getEmail());
-                        System.out.println("\tSenha: " + uTemp.getSenha());
-                        encontrado = true;
-                        break;
+                        System.out.println("\tNomes: " + uTemp.getNome());
+                        System.out.println("\tCPF: " + uTemp.getCpf());
                     }
+                    break;
                 }
-                if (!encontrado) {
-                    System.out.println("USUÁRIO NÃO ENCONTRADO!");
+                case "3": {
+                    System.out.println("==========PROCURAR USUÁRIO POR EMAIL==========");
+                    System.out.println("Digite o email:");
+                    String email = input.nextLine();
+                    boolean encontrado = false;
+                    for (int i = 0; i < usuarios.size(); i++) {
+                        Usuario uTemp = usuarios.get(i);
+                        if (email.equals(uTemp.getEmail())) {
+                            System.out.println("Usuário encontrado");
+                            System.out.println("\tNome: " + uTemp.getNome());
+                            System.out.println("\tEmail: " + uTemp.getEmail());
+                            System.out.println("\tSenha: " + uTemp.getSenha());
+                            encontrado = true;
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("USUÁRIO NÃO ENCONTRADO!");
+                    }
+                    break;
                 }
-                break;
-            }
-            case "4": {
-                System.out.println("ATUALIZAÇÃO DE USUÁRIO");
-                
-                for (int i = 0; i < usuarios.size(); i++) {
-                    Usuario uTemp = usuarios.get(i);  
-                    System.out.println("["+i+"] " + uTemp.getEmail());
+                case "4": {
+                    System.out.println("==========ATUALIZAÇÃO DE USUÁRIO==========");
+
+                    for (int i = 0; i < usuarios.size(); i++) {
+                        Usuario uTemp = usuarios.get(i);
+                        System.out.println("[" + i + "] " + uTemp.getEmail());
+                    }
+
+                    System.out.println("Digite o numero do usuario!");
+                    int numUser = input.nextInt();
+                    input.nextLine(); // Limpa o buffer
+                    System.out.println("Digite o novo Email: ");
+                    String novoEmail = input.nextLine();
+                    System.out.println("Digite a nova Senha: ");
+                    String novaSenha = input.nextLine();
+
+                    Usuario u = usuarios.get(numUser);
+                    u.setEmail(novoEmail);
+                    u.setSenha(novaSenha);
+                    break;
                 }
-                
-                System.out.println("Digite o numero do usuario!");
-                int numUser = input.nextInt();
-                input.nextLine(); // Limpa o buffer
-                System.out.println("Digite o novo Email: ");
-                String novoEmail = input.nextLine();
-                System.out.println("Digite a nova Senha: ");
-                String novaSenha = input.nextLine();
-                
-                Usuario u = usuarios.get(numUser);
-                u.setEmail(novoEmail);
-                u.setSenha(novaSenha);
-                break;
-            }
-            case "5": {
-                System.out.println("REMOVER USUÁRIO");
-                for (int i = 0; i < usuarios.size(); i++) {
-                    Usuario uTemp = usuarios.get(i);  
-                    System.out.println("["+i+"] " + uTemp.getEmail());
+                case "5": {
+                    System.out.println("==========REMOVER USUÁRIO==========");
+                    for (int i = 0; i < usuarios.size(); i++) {
+                        Usuario uTemp = usuarios.get(i);
+                        System.out.println("[" + i + "] " + uTemp.getEmail());
+                    }
+                    System.out.println("Digite o numero do usuario!");
+                    int numUser = input.nextInt();
+                    input.nextLine(); // Limpa o buffer
+                    usuarios.remove(numUser);
+                    break;
                 }
-                System.out.println("Digite o numero do usuario!");
-                int numUser = input.nextInt();
-                input.nextLine(); // Limpa o buffer
-                usuarios.remove(numUser);
-                break;
+                case "6": {
+                    System.out.println("==================================");
+                    System.out.println("Sistema Finalizado!");
+                    rodando = false;
+                    break;
+                }
+                default:
+                    System.out.println("==================================");
+                    System.out.println("Opção inválida. Digite outra opção:");
+                    opcao = input.nextLine();
+
+                    while (Integer.parseInt(opcao) > 6 || Integer.parseInt(opcao) < 1) {
+                        System.out.println("Opção inválida. Digite outra opção:");
+                        opcao = input.nextLine();
+                    }
+                    break;
             }
-            case "6": {
-                System.out.println("Finalizando Sistema!");
-                rodando = false;
-                break;
-            }
-            default:
-                System.out.println("opção inválida digite outra opção :");
-                opcao = input.nextLine();
-                break;        
         }
-    }    
-                 
+
     }
 }
--- src/main/java/com/mycompany/principal/java/Usuario.java
+++ src/main/java/com/mycompany/principal/java/Usuario.java
@@ -11,6 +11,11 @@ package com.mycompany.principal.java;
 public class Usuario {
     private String email;
     private String senha;
+    private String nome;
+    private String idade;
+    private String cpf;
+   
+   
 
     public String getEmail() {
         return email;
@@ -27,6 +32,31 @@ public class Usuario {
     public void setSenha(String senha) {
         this.senha = senha;
     }
-    
 
-}
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+   
+   
+
+}   
